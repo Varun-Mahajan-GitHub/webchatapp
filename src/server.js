b9ee0b7000ad74406c132d3882363b4754d2710d
Varun Mahajan <varun.mahajan@saavn.com>
2023-10-06 21:42:40 +0530

    backend and simple fe for a web chat app

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(express.static('public'))
+wss.on('connection', (ws) => {
+  console.log('A new user connected');
+
+  ws.on('message', (message) => {
+    console.log('Received message:', message);
+    // Broadcast the message to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
